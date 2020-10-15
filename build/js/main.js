688c0d649180fe482bd2c91bc9d7f2ea39eac323
Victoria Domovets <v.domovets@gmail.com>
2020-10-15 22:45:51 +0200

    правки js

--- build/js/main.js
+++ build/js/main.js
@@ -7,7 +7,9 @@ var burger = document.querySelector('.burger');
 
 headerBody.classList.add('header__body--js');
 
-burger.addEventListener('click', function () {
-  headerBody.classList.toggle('header__body--js');
-  burger.classList.toggle('active');
-});
+if (burger) {
+  burger.addEventListener('click', function () {
+    headerBody.classList.toggle('header__body--js');
+    burger.classList.toggle('active');
+  });
+}
